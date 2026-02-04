69fadd99d8347207d851756ac5330c218661b30d
syedomer17 <syedomerali2006@gmail.com>
2026-02-04 18:03:04 +0530

    Updated by Omer on 2026-02-04 at 06:03PM

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from "path";
 import { fileURLToPath } from "url";
+import { Request, Response } from 'express';
 
 dotenv.config();
 
@@ -30,8 +31,8 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not Found Router" });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get(/(.*)/, (req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
